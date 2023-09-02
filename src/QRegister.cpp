3cdac995e20346575c0b31c2d41ecb424402d67f
Vincent Gierisch <vincentgierisch@gmail.com>
2023-09-02 13:12:00 +0200

    fix reg length

--- src/QRegister.cpp
+++ src/QRegister.cpp
@@ -1,8 +1,10 @@
 #include "QRegister.h"
 
 QRegister::QRegister(unsigned int length, unsigned int value):_lenght(length) {
-    this->_register = Eigen::Vector<bool, Eigen::Dynamic>(2);
-    for(unsigned int i = 0; i < length; ++i) {
-        this->_register[i] = (length == i+1?1:0);
+    unsigned int lengthOfRegister = pow(2, length);
+    this->_register = Eigen::Vector<bool, Eigen::Dynamic>(lengthOfRegister);
+    assert(value < lengthOfRegister);
+    for(unsigned int i = 0; i < lengthOfRegister; ++i) {
+        this->_register[i] = (value == i?1:0);
     }
 }
